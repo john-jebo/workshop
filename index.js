ee5c32828c2d3e57ae27e8ba0afe5633b14e35ca
Ernest Amuzu <ernestamuzu@ymail.com>
2021-04-27 14:38:07 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-console.log('good');
+console.log("good");
 
 // window.location.assign('/gold');
 
-const http = require('http');
+const http = require("http");
 
 http.get("Fdfd");
+
+//hello world
