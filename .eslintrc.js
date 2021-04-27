7558945f7c9df8ca5feeaa9694a72fe9edb1013e
John Jebo <john.jebo@amalitech.com>
2021-04-27 13:31:59 +0000

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "env": {
+        // "browser": true,
+        // "commonjs": true,
+        // "es2021": true
+        // "browser": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 12
+    },
+    "rules": {
+        "semi": ["error", "always"]
+    },
+    "globals": {
+        Form: true,
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    }
+};
---
+++ index.js
@@ -0,0 +1,7 @@
+console.log('good');
+
+// window.location.assign('/gold');
+
+const http = require('http');
+
+http.get("Fdfd");
